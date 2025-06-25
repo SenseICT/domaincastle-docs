8247e1e67ef0656341d1355518250a604733bbcf
Matt Spurrier <matthew@senseict.com.au>
2025-06-25 21:38:31 +0800

    update deploy workflows and dependabot config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'senseict', // Usually your GitHub org/user name.
   projectName: 'domaincastle-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
